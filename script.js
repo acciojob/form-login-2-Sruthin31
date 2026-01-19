0cfa6729c1486d713d648fed6e2a9aa713e1e29a
Sruthin31 <153161309+Sruthin31@users.noreply.github.com>
2026-01-19 07:50:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 //your JS code here. If required.
-const Btn = document.getElementById("submit");
+document.addEventListener("DOMContentLoaded", () =>{
+	const form = document.getElementById("userForm");
 
-Btn.addEventListener("click",() => {
-	const Firstname = document.getElementById("fname").value;
+	form.addEventListener("submit",(e) =>{
+		e.preventDefault();
+
+		const Firstname = document.getElementById("fname").value;
 const Lastname = document.getElementById("lname").value;
 const Phoneno = document.getElementById("phone").value;
 const Email = document.getElementById("email").value;
@@ -11,4 +14,5 @@ const Email = document.getElementById("email").value;
 	const messsage = `First Name:${Firstname}\nLast Name:${Lastname}\nPhone Number:${Phoneno}\nEmail Id:${Email}`;
 
 	alert(messsage);
+	});
 });
